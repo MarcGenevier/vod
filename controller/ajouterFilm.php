6b7adf4a1e7c9dafa546f084985bd198c143ff27
Marc Genevier <marc.genevier.sio@gmail.com>
2020-05-06 14:05:04 +0200

    gestion ajout film

---
+++ controller/ajouterFilm.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+    <?php
+
+        $film = $_POST["titre"].":".$_POST["annee"].":".$_POST["realisateur"];
+        $films = fopen("../data/vod.csv", "a");
+        fwrite($films, $film);
+        fclose($films);
+
+        echo "Le film a été ajouté avec succés ".$_POST["titre"].":".$_POST["annee"].":".$_POST["realisateur"];
+
+    ?> 
+    </body>
+</html>
