0905ad31c00804f068511590bece990920ae91ba
Marc Genevier <marc.genevier.sio@gmail.com>
2020-05-06 15:36:04 +0200

    Ajout liste des films

---
+++ view/consulterFilms.php
@@ -0,0 +1,30 @@
+<html>
+  <body>
+      <table>
+          <thead>
+              <tr>
+                  <td>Titre</td>
+                  <td>Année</td>
+                  <td>Réalisateur</td>
+              </tr>
+          </thead>
+          <tbody>
+            <?php
+            $path = "../data/vod.csv" ;
+            $films = file($path);
+            
+            foreach($films as $n => $film){
+                list($titre, $annee, $realisateur) = explode(":", $film);
+            ?>
+                <tr>
+                    <td><?php echo $titre ?></td>
+                    <td><?php echo $annee ?></td>
+                    <td><?php echo $realisateur ?></td>
+                </tr>
+            <?php
+            }
+            ?>
+          </tbody>
+      </table>
+  </body>
+</html>
