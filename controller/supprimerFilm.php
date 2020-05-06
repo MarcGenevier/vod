d1a59f7d00b4ff48c123d909c448eb4b701e714a
Marc Genevier <marc.genevier.sio@gmail.com>
2020-05-06 15:43:47 +0200

    Correction indentation

--- controller/supprimerFilm.php
+++ controller/supprimerFilm.php
@@ -9,11 +9,11 @@
       list($titre, $annee, $realisateur) = explode(":", $film);
 
       if(trim($titre) == trim($_POST["titre"])){
-	$filmsContent = file_get_contents($path);
+	      $filmsContent = file_get_contents($path);
         $filmsContent = str_replace($film, "", $filmsContent);
         file_put_contents($path, $filmsContent);
 
-	echo "Le film ".$titre." a été supprimé avec succès";
+	      echo "Le film ".$titre." a été supprimé avec succès";
         break;
       }
     }
